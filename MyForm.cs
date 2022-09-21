171722f615fb2ae35d8fd835d0370ff4d77d5b49
Alireza Sadeghi <arsadeqi1@mail.com>
2022-09-21 12:27:08 +0430

    Add project files.

---
+++ MyForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08_بررسي_اشيا_فرم_ها
+{
+    public class MyForm : Form
+    {
+        public MyForm()
+        {
+            var btn = new Button();
+            btn.Text = "Click Me";
+            btn.Click += (s, e) => { this.Close(); };
+            this.Controls.Add(btn);
+            btn.Top = 20;
+            btn.Left = 40;
+            this.Text = "Alireza The Programmer!!!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace _08_بررسي_اشيا_فرم_ها
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var frm = new MyForm();
+            frm.Show();
+            frm.FormClosed += (s, e) =>
+            {
+                MessageBox.Show("You're closing the whole program buddy!!!");
+                Application.Exit();
+            };
+
+            Application.Run();
+        }
+    }
+}
